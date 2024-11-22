277891530bae6abbc0c0054ac3c4cfc5a81116ef
Le Van Quy <vannn.quy@gmail.com>
2024-11-22 18:25:42 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT', 8080);
+  const frontendUrl = configService.get<string>('FRONTEND_URL', 'http://localhost:3000'); // Địa chỉ của frontend
+
+  // Cấu hình CORS cho phép yêu cầu từ frontend
+  app.enableCors({
+    origin: frontendUrl, // Lấy từ biến môi trường
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
 
   await app.listen(port);
   console.log(`🚀 Ứng dụng đang chạy tại: http://localhost:${port}`);
